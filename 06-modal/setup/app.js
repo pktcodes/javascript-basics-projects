cc443f59c548a8b1c6116f2b7c231f1eb5bc7cbb
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-23 23:39:11 -0600

    06 - Modal JS

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal-overlay");
+
+modalBtn.addEventListener("click", function () {
+  console.log(modal.classList);
+  modal.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
