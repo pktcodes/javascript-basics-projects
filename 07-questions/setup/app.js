0b890c0a6055703828a971e2871fcea2502b0b7e
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-25 15:54:55 -0600

    07 - Questions JS - Using Selectors Inside the Element

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,11 +1,31 @@
-// using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+/* const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (event) {
     const question = event.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+}); */
+
+// using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  //   console.log(question);
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    // console.log(btn);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        console.log(question);
+        item.classList.remove("show-text");
+      }
+    });
+
     question.classList.toggle("show-text");
   });
 });
