74f3eb22d7548923a5b5ba040421eb90bfb71576
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-24 23:11:36 -0600

    07 - Questions JS - Traversing the DOM

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,11 @@
-//using selectors inside the element
+// using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    const question = event.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
