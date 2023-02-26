df3ca6d20349f3d5247bdb45a4714f78e0d0147c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-26 14:50:22 -0600

    08 - Menu - Unique Categories

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -92,6 +92,16 @@ const filterBtns = document.querySelectorAll(".filter-btn");
 // load items
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menu);
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  console.log(categories);
 });
 
 // filter items
