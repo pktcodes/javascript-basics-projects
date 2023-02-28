c44075b9370b14d493b6b3771884cd3d19975e76
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-02-28 00:59:20 -0600

    10 - Scroll - Toggle Simple Setup

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -8,6 +8,13 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+});
 
 // ********** fixed navbar ************
 
