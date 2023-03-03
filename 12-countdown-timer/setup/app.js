6f7e118c7189b3508c3b680f5b4bc726648b924d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-02 23:34:13 -0600

    12 - Countdown - Set Date

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,22 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const countdownItems = document.querySelectorAll(".deadline-format h4");
+console.log(countdownItems);
+
+const futureDate = new Date(2023, 3, 4, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `giveway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
