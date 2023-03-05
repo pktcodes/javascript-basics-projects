7056d1c7865d36a948f0f6680a99e7edfc946688
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-04 22:56:08 -0600

    14 - GroceryBud - Select Elements

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,7 +1,16 @@
 // ****** SELECT ITEMS **********
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const input = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
-
+let editElement;
+let editFlag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
 
 // ****** FUNCTIONS **********
