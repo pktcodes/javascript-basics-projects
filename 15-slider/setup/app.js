703d6b9fdb73f409dfa680fe0c0254a5e3cd7dc1
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-10 00:04:52 -0600

    15 - Slider JS - Slides Approach

--- 15-slider/setup/app.js
+++ 15-slider/setup/app.js
@@ -5,3 +5,27 @@ const nextBtn = document.querySelector(".nextBtn");
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}% `;
 });
+
+let counter = 0;
+
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  if (counter > slides.length - 1) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
