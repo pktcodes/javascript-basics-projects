ef143eb4f1b60dfce082a82c0b45c350d4814748
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-17 17:22:20 -0500

    16 - Counter - Functions

--- 16-counter/starter/app.js
+++ 16-counter/starter/app.js
@@ -1,17 +1,4 @@
-function Counter(element, value) {
-  this.counter = element;
-  this.value = value;
-  this.decrease = element.querySelector(".decrease");
-  this.reset = element.querySelector(".reset");
-  this.increase = element.querySelector(".increase");
-  this.valueDOM = element.querySelector(".value");
-  this.valueDOM.textContent = this.value;
-  console.log(this);
-}
-
-const firstCounter = new Counter(getElement(".first-counter"), 100);
-const secondCounter = new Counter(getElement(".second-counter"), 200);
-
+/* Helper Function */
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -21,3 +8,41 @@ function getElement(selection) {
     `Please check the ${selection} selector, no such element exists`
   );
 }
+
+/* Constructor Function */
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+}
+
+/* Prototype */
+Counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+/* Creating Instances */
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+firstCounter.increase();
+firstCounter.increase();
+firstCounter.increase();
+firstCounter.decrease();
+secondCounter.reset();
