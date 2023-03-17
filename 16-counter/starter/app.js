bda28999fe0c62a67e957b2032604a17f626ea1d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-17 15:55:33 -0500

    16 - Counter - Setup

---
+++ 16-counter/starter/app.js
@@ -0,0 +1,16 @@
+function Counter(element, value) {
+  console.log(element, value);
+}
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check the ${selection} selector, no such element exists`
+  );
+}
