691a2c8e30f47f36a08525cc1244f3d2b7f505ec
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-03-17 23:38:29 -0500

    17 - Gallery - Select Elements

--- 17-gallery/starter/app.js
+++ 17-gallery/starter/app.js
@@ -7,3 +7,19 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  // Making Array from NodeList using Spread Operator
+  this.list = [...element.querySelectorAll(".img")];
+
+  // Modal is in document, not in element
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.prevBtn = getElement(".prev-btn");
+  this.nextBtn = getElement(".next-btn");
+}
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
