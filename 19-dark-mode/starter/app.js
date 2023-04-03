3bb62e5087aa16c743f1df25992c1a7ad586091c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-02 23:09:12 -0500

    19 - Dark Mode - Render Items

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,5 +1,25 @@
 const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
 
 toggleBtn.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-theme");
 });
+
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+
+    // format date
+
+    return `<article class="post">
+              <h2>${title}</h2>
+              <div class="post-info">
+                <span>${date}</span>
+                <span>${length} min read</span>
+              </div>
+              <p>${snippet}</p>
+            </article>`;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articlesData;
