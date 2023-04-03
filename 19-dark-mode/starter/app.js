7800939b5f089ba631bd8ec18d8f4b662597b393
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-03 00:02:40 -0500

    19 - Dark Mode - Moment.js

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -10,11 +10,12 @@ const articlesData = articles
     const { title, date, length, snippet } = article;
 
     // format date
+    const formatDate = moment(date).format("MMMM Do YYYY");
 
     return `<article class="post">
               <h2>${title}</h2>
               <div class="post-info">
-                <span>${date}</span>
+                <span>${formatDate}</span>
                 <span>${length} min read</span>
               </div>
               <p>${snippet}</p>
