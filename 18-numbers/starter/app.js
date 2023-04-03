cee129cc89e202ca581fa3b0f4b2d9efcee9f054
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-02 20:28:40 -0500

    18 - Numbers - Complete

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -3,8 +3,20 @@ const numbers = [...document.querySelectorAll(".number")];
 const updateCount = (element) => {
   const value = parseInt(element.dataset.value);
   const increment = Math.ceil(value / 1000);
+  // const increment = 1;
   let initialValue = 0;
-  console.log(increment);
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    if (initialValue > value) {
+      element.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    element.textContent = `${initialValue}+`;
+  }, 1);
+
+  console.log(increaseCount);
 };
 
 numbers.forEach((item) => {
