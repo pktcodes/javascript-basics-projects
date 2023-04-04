2016ee992028579170ffb24a1495cfa48d433d5c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-03 23:02:04 -0500

    20 - Filters - Display Products

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,1 +1,24 @@
-console.log('filters project');
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector(".products-container");
+
+const displayProducts = () => {
+  // more code - if no matching products
+  productsContainer.innerHTML = filteredProducts
+    .map((product) => {
+      const { id, title, image, price } = product;
+      return `<article class="product" data-id="${id}">
+                <img
+                  src="${image}"
+                  class="img product-img"
+                />
+                <footer>
+                  <h5 class="product-name">${title}</h5>
+                  <span class="product-price">$${price}</span>
+                </footer>
+              </article>`;
+    })
+    .join("");
+};
+
+displayProducts();
