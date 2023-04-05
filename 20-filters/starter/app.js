79828e7e933d6a574371e0aaa4ca0efabf2a5cae
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-04 23:02:31 -0500

    20 - Filters - Text Filter

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -4,9 +4,11 @@ const productsContainer = document.querySelector(".products-container");
 
 const displayProducts = () => {
   // more code - if no matching products
+
   productsContainer.innerHTML = filteredProducts
-    .map((product) => {
-      const { id, title, image, price } = product;
+    .map(({ id, title, image, price }) => {
+      // const { id, title, image, price } = product;
+
       return `<article class="product" data-id="${id}">
                 <img
                   src="${image}"
@@ -22,3 +24,23 @@ const displayProducts = () => {
 };
 
 displayProducts();
+
+/* Filter Based on Search Text */
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
+form.addEventListener("keyup", () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue.toLowerCase());
+  });
+  console.log(filteredProducts);
+  displayProducts();
+});
+
+/* includes("") - gives true */
+// console.log(
+//   products.filter((product) => {
+//     return product.title.toLowerCase().includes("");
+//   })
+// );
