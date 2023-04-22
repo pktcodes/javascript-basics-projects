e11aa9dc5fbe33bbc045e69612cfb44e9d88d49d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-21 23:59:43 -0500

    21 - Dad Jokes - Fetch Random Joke

--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -5,4 +5,15 @@ const result = document.querySelector(".result");
 
 btn.addEventListener("click", () => {
   console.log("click");
+  fetchJoke();
 });
+
+async function fetchJoke() {
+  const response = await fetch(url, {
+    headers: { accept: "application/json", "user-agent": "string" },
+  });
+  const data = await response.json();
+  result.textContent = data.joke;
+}
+
+fetchJoke();
