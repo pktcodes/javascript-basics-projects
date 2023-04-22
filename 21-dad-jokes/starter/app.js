aa22acdd17b9e181924cc5e8ad1dc636ff4a15c9
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-21 23:19:39 -0500

    21 - Dad Jokes - Select Elements

--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -1,1 +1,8 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/";
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  console.log("click");
+});
