7cb654d65891f3e72132b21cd5626b74afd6e06f
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-22 22:48:08 -0500

    21 - Dad Jokes - Loading

--- 21-dad-jokes/starter/app.js
+++ 21-dad-jokes/starter/app.js
@@ -4,11 +4,11 @@ const btn = document.querySelector(".btn");
 const result = document.querySelector(".result");
 
 btn.addEventListener("click", () => {
-  console.log("click");
   fetchJoke();
 });
 
 async function fetchJoke() {
+  result.textContent = "Loading...";
   const response = await fetch(url, {
     headers: { accept: "application/json", "user-agent": "string" },
   });
