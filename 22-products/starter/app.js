4ad571cfb0d929e280d58385ecd3e03dbaa24a5c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-24 22:14:10 -0500

    22 - Products - Fetch Products

--- 22-products/starter/app.js
+++ 22-products/starter/app.js
@@ -1,1 +1,15 @@
-console.log('products starter');
+const url = "https://course-api.com/javascript-store-products";
+
+const productsDOM = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchProducts();
