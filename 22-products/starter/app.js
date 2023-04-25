400eda56f19a2b7cd5f2ba837fce61c423dd5831
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-24 22:53:39 -0500

    22 - Products - Start Function

--- 22-products/starter/app.js
+++ 22-products/starter/app.js
@@ -1,16 +1,28 @@
-const url = "https://course-api.com/javascript-store-productss";
+const url = "https://course-api.com/javascript-store-products";
 
 const productsDOM = document.querySelector(".products-center");
 
+// Fetching the products
 const fetchProducts = async () => {
   productsDOM.innerHTML = `<div class="loading"></div>`;
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     productsDOM.innerHTML = `<p class="error">there was an error</p>`;
   }
 };
 
-fetchProducts();
+// Display the products
+const displayProducts = (list) => {
+  console.log(list);
+};
+
+// Start Function
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+start();
