8ea29df2bf7bba769b86efff9102649fb454b6b7
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-24 22:27:04 -0500

    22 - Products - Loading and Error

--- 22-products/starter/app.js
+++ 22-products/starter/app.js
@@ -1,14 +1,15 @@
-const url = "https://course-api.com/javascript-store-products";
+const url = "https://course-api.com/javascript-store-productss";
 
 const productsDOM = document.querySelector(".products-center");
 
 const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
   try {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
   } catch (error) {
-    console.log(error);
+    productsDOM.innerHTML = `<p class="error">there was an error</p>`;
   }
 };
 
