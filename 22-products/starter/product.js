ed45ca674480a22facb48f4bad62381640e4eefa
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-26 22:59:10 -0500

    22 - Products - Fetch Product

--- 22-products/starter/product.js
+++ 22-products/starter/product.js
@@ -1,10 +1,24 @@
 const productDOM = document.querySelector(".product");
 
-const url =
-  "https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog";
+const url = "https://course-api.com/javascript-store-single-product";
 
 const fetchProduct = async () => {
-  return "hello, i am product";
+  try {
+    productDOM.innerHTML = `<h4 class="product-loading">Loading...</h4>`;
+
+    /* Gets the query params string */
+    // console.log(window.location.search);
+
+    /* Gets the value of the key from query params of the URL */
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML = `<p class="error">There was an error looking for the product. Please try again later</p>`;
+  }
 };
 
 const displayProduct = (data) => {
