23158f676064d05fe87fb2faa80557bd5641128b
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-29 20:53:28 -0500

    24 - Cocktails - Present Drinks

--- 24-cocktails/setup/app.js
+++ 24-cocktails/setup/app.js
@@ -1,1 +1,7 @@
+import presentDrinks from "./src/presentDrinks.js";
+
 const URL = "https://thecocktaildb.com/api/json/v1/1/search.php?s=a";
+
+window.addEventListener("DOMContentLoaded", () => {
+  presentDrinks(URL);
+});
---
+++ 24-cocktails/setup/src/presentDrinks.js
@@ -0,0 +1,8 @@
+const showDrinks = async (url) => {
+  console.log(url);
+
+  // Get Drinks
+  // Display Drinks
+};
+
+export default showDrinks;
