fbe479f5bfe30b3e85030deb1fec45c1bfe58cce
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-01 00:14:33 -0500

    24 - Cocktails - Single Drink - Ingredients Logic

--- 24-cocktails/setup/src/displaySingleDrink.js
+++ 24-cocktails/setup/src/displaySingleDrink.js
@@ -6,27 +6,33 @@ const displayDrink = (data) => {
 
   const drink = data.drinks[0];
   const { strDrink: name, strDrinkThumb: image, strInstructions: desc } = drink;
-  const ingredientsList = [
-    drink.strIngredient1,
-    drink.strIngredient2,
-    drink.strIngredient3,
-    drink.strIngredient4,
-    drink.strIngredient5,
-  ];
 
+  /*   Alternative Solution to Get All Ingredients */
+  const list = Object.entries(drink).filter((item) => {
+    const [key, value] = item;
+    if (key.includes("Ingredient") && value) {
+      return item;
+    }
+  });
+  const ingredientsList = list.map((item) => {
+    return item[1];
+  });
+
+  // Select Elements
   const img = get(".drink-img");
   const title = get(".drink-name");
   const description = get(".drink-desc");
   const ingredients = get(".drink-ingredients");
 
+  // Title
   document.title = name;
 
+  // Set Data
   img.src = image;
   title.textContent = name;
   description.textContent = desc;
   ingredients.innerHTML = ingredientsList
     .map((item) => {
-      if (!item) return;
       return `<li><i class="far fa-check-square"></i>${item}</li>`;
     })
     .join("");
