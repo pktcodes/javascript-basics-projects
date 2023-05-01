061fe6e348e207b8daa14cceab862d1f7d5241fa
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-30 19:34:50 -0500

    24 - Cocktails - Loading

--- 24-cocktails/setup/src/displayDrinks.js
+++ 24-cocktails/setup/src/displayDrinks.js
@@ -1,10 +1,12 @@
 import get from "./getElement.js";
+import { hideLoading } from "./toggleLoading.js";
 
 const title = get(".title");
 const section = get(".section-center");
 
 const displayDrinks = ({ drinks }) => {
   if (!drinks) {
+    hideLoading();
     title.textContent = `sorry, no drinks matched your search`;
     section.textContent = null;
     return;
@@ -23,6 +25,7 @@ const displayDrinks = ({ drinks }) => {
             </a>`;
     })
     .join("");
+  hideLoading();
   title.textContent = "";
   section.innerHTML = newDrinks;
   return section;
