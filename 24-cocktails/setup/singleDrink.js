503686a855a5a775756bf1ed49b63260c6fb7c45
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-04-30 22:31:30 -0500

    24 - Cocktails - Single Drink

---
+++ 24-cocktails/setup/singleDrink.js
@@ -0,0 +1,16 @@
+import displayDrink from "./src/displaySingleDrink.js";
+import fetchDrinks from "./src/fetchDrinks.js";
+
+const presentDrink = async () => {
+  const id = localStorage.getItem("drink");
+  if (!id) {
+    window.location.replace("index.html");
+  } else {
+    const drink = await fetchDrinks(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+    displayDrink(drink);
+  }
+};
+
+window.addEventListener("DOMContentLoaded", presentDrink);
---
+++ 24-cocktails/setup/src/displaySingleDrink.js
@@ -0,0 +1,5 @@
+const displayDrink = (drink) => {
+  console.log(drink);
+};
+
+export default displayDrink;
