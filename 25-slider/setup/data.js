9e54f80975d12fe1e16e9553e4d11950301fada5
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-04 23:35:11 -0500

    25 - Slider - Apply CSS

--- 25-slider/setup/data.js
+++ 25-slider/setup/data.js
@@ -17,6 +17,12 @@ const people = [
     job: "designer",
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
+  {
+    img: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+    name: "susan doe",
+    job: "developer",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
 ];
 
 export default people;
