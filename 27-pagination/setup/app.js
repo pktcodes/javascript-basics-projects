16e378079408021ab29e0d625cfa20c1f69c5c44
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-06 17:24:21 -0500

    27 - Pagination - Display Followers

--- 27-pagination/setup/app.js
+++ 27-pagination/setup/app.js
@@ -3,9 +3,12 @@ import displayFollowers from "./displayFollowers.js";
 import paginate from "./paginate.js";
 import displayButtons from "./displayButtons.js";
 
+const title = document.querySelector(".section-title h1");
+
 const init = async () => {
   const followers = await fetchFollowers();
-  console.log(followers);
+  title.textContent = "pagination";
+  displayFollowers(followers);
 };
 
 window.addEventListener("load", init);
--- 27-pagination/setup/displayFollowers.js
+++ 27-pagination/setup/displayFollowers.js
@@ -1,3 +1,18 @@
-const display = () => {}
+const container = document.querySelector(".container");
 
-export default display
+const display = (followers) => {
+  const newFollowers = followers
+    .map((person) => {
+      const { avatar_url, html_url, login } = person;
+      return `<article class="card">
+                <img src="${avatar_url}"/>
+                <h4>${login}</h4>
+                <a href="${html_url}" class="btn" target="_blank">view profile</a> 
+              </article>`;
+    })
+    .join("");
+
+  container.innerHTML = newFollowers;
+};
+
+export default display;
