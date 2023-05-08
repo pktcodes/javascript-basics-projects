45f97b63533d23271213a471db4efdd3eba8b5cf
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-07 20:12:11 -0500

    28 - Wikipedia API -  Initial Setup

--- 28-wikipedia/starter/app.js
+++ 28-wikipedia/starter/app.js
@@ -1,4 +1,22 @@
 const url =
   "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=";
 
-console.log("wiki starter");
+const formDOM = document.querySelector(".form");
+const inputDOM = document.querySelector(".form-input");
+const resultsDOM = document.querySelector(".results");
+
+formDOM.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML = `<div class="error">
+                                please enter valid search term
+                            </div>`;
+    return;
+  }
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+  console.log(searchValue);
+};
