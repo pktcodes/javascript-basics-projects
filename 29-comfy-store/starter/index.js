af6ee413f4252017ddd467c9b8ae2e588ec66226
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-13 22:34:02 -0500

    29 - Comfy Store - Fetch Products

--- 29-comfy-store/starter/index.js
+++ 29-comfy-store/starter/index.js
@@ -7,3 +7,10 @@ import fetchProducts from "./src/fetchProducts.js";
 import { setupStore, store } from "./src/store.js";
 import display from "./src/displayProducts.js";
 import { getElement } from "./src/utils.js";
+
+const init = async () => {
+  const products = await fetchProducts();
+  console.log(products);
+};
+
+window.addEventListener("DOMContentLoaded", init);
--- 29-comfy-store/starter/src/fetchProducts.js
+++ 29-comfy-store/starter/src/fetchProducts.js
@@ -1,5 +1,11 @@
 import { allProductsUrl } from "./utils.js";
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  const response = await fetch(allProductsUrl).catch((err) => console.log(err));
+  if (response.ok && response.status === 200) {
+    return response.json();
+  }
+  return response;
+};
 
 export default fetchProducts;
