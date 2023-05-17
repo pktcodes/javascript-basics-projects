f8b756cc63f54e4eb05c99acc16c93f3ca351106
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-16 19:45:18 -0500

    29 - Comfy Store - Display All Products

--- 29-comfy-store/starter/src/pages/products.js
+++ 29-comfy-store/starter/src/pages/products.js
@@ -1,14 +1,16 @@
 // global imports
-import '../toggleSidebar.js';
-import '../cart/toggleCart.js';
-import '../cart/setupCart.js';
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 
 //  filter imports
-import setupSearch from '../filters/search.js';
-import setupCompanies from '../filters/companies.js';
-import setupPrice from '../filters/price.js';
+import setupSearch from "../filters/search.js";
+import setupCompanies from "../filters/companies.js";
+import setupPrice from "../filters/price.js";
 
 // specific imports
-import { store } from '../store.js';
-import display from '../displayProducts.js';
-import { getElement } from '../utils.js';
+import { store } from "../store.js";
+import display from "../displayProducts.js";
+import { getElement } from "../utils.js";
+
+display(store, getElement(".products-container"));
