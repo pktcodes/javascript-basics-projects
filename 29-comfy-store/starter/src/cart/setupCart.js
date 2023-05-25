c7683bc54093cc004fe2a450f1dc71324fb48c86
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-24 22:33:06 -0500

    29 - Comfy Store -  setupCart - Initial Setup

--- 29-comfy-store/starter/src/cart/setupCart.js
+++ 29-comfy-store/starter/src/cart/setupCart.js
@@ -10,7 +10,19 @@ import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
 
+const cartItemCount = getElement(".cart-item-count");
+const cartItems = getElement(".cart-items");
+const cartTotal = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
+
 export const addToCart = (id) => {
   console.log(id);
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+
+init();
