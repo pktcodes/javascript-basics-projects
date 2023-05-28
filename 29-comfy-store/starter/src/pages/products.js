61126eba3d57b917f2828b36ec0cdb076268d883
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-05-28 02:13:03 -0500

    29 - Comfy Store - BugFix - Initial Products Store

--- 29-comfy-store/starter/src/pages/products.js
+++ 29-comfy-store/starter/src/pages/products.js
@@ -9,17 +9,28 @@ import setupCompanies from "../filters/companies.js";
 import setupPrice from "../filters/price.js";
 
 // specific imports
-import { store } from "../store.js";
+import { setupStore, store } from "../store.js";
 import display from "../displayProducts.js";
 import { getElement } from "../utils.js";
 
-const loading = getElement(".page-loading");
+// import fetchProducts
+import fetchProducts from "../fetchProducts.js";
 
-display(store, getElement(".products-container"));
+const init = async () => {
+  const loading = getElement(".page-loading");
+  if (store.length < 1) {
+    const products = await fetchProducts();
+    setupStore(products);
+  }
 
-// Search
-setupSearch(store);
-setupCompanies(store);
-setupPrice(store);
+  display(store, getElement(".products-container"));
 
-loading.style.display = "none";
+  // Search
+  setupSearch(store);
+  setupCompanies(store);
+  setupPrice(store);
+
+  loading.style.display = "none";
+};
+
+init();
